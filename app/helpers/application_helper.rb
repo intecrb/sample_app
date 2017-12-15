53731907c02e1daa18de8d5152adf9611250f31e
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2017-12-15 12:22:36 +0900

    4章を完了した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    # .empty?で文字列が空かどうか
+    # .nil?でobjectそのものが空かどうか
+    # .blank?で...?
+    if page_title.empty?
+      base_title
+    else
+      # page_title + "|" + base_title
+      # + で文字列はジョインしないほうがイケてる
+      "#{page_title}|#{base_title}"
+    end
+  end
 end
