385d7ff07c45f305da9ffd12c922256dd81d13d6
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-02-07 12:55:32 +0900

    7.3の演習2番まで完了

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    # ユーザ登録した前後でUserの数が変わってないか？
+    assert_no_difference 'User.count' do
+      post users_path,
+      params: {
+        user: {
+          name:  "",
+          email: "user@invalid",
+          password:              "foo",
+          password_confirmation: "bar"
+        }
+      }
+    end
+    # エラーメッセージが正しく表示されているか？
+    assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
+  end
+end
