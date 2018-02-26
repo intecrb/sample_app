2c1e53846adb46c5ff6304319afeac56d8775654
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-02-26 12:41:18 +0900

    8.1.3ログインの際のif文を作った

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # メールアドレスはdowncaseで登録してあります
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザログイン後にユーザ情報のページにリダイレクトする
+    else
+      # エラーメッセージを表示する
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
