4c94d685749e3d415b00107350b2a88d74f18c53
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2018-03-05 12:33:43 +0900

    8.2.4 有効なユーザでのログインのテストまで実装した

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,5 +4,15 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
-  
+
+  # 現在ログインしているユーザを返す
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
+  def logged_in?
+    !current_user.nil?
+  end
+
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,6 +1,33 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
+  # 有効なアカウントではloginできないことを確認するテスト
+  test "login with valid information" do
+    # /loginの画面ですか？
+    get login_path
+    post login_path,
+    params: {
+      session: {
+        email:    @user.email,
+        password: 'password'
+      }
+    }
+    # リダイレクト先が正しいですか？
+    assert_redirected_to @user
+    # 実際にリダイレクトする
+    follow_redirect!
+    assert_template 'users/show'
+    # login_pathがないのでログイン画面ではない
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
   # 無効なアカウントではloginできないことを確認するテスト
   test "login with invalid information" do
     get login_path
