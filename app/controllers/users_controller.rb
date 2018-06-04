6a78e396356c469286cce1334d7ec67d32a184de
awaji-daisuke <awaji_daisuke@intec.co.jp>
2018-06-04 12:31:16 +0900

    10.4.1まで完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-      :password_confirmation)
+      :password_confirmation, :admin)
   end
 
   # ログイン済みユーザーかどうか確認
