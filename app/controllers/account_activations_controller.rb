2669882ce93fb32faab74db66acd5738d4f056c4
awaji-daisuke <awaji_daisuke@intec.co.jp>
2018-06-13 13:00:51 +0900

    11.1.2 AccountActivationのデータモデルまで完了

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,8 @@
+class AccountActivationsController < ApplicationController
+
+
+private
+  def create_activation_digest
+    # 有効化トークンとダイジェストを作成および代入する
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
   # emailはDBに保存する前に小文字にする
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -46,4 +48,16 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+private
+
+  # メールアドレスをすべて小文字にする
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 有効化トークンとダイジェストを作成および代入する
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
 end
