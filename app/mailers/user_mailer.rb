31f4a02bf3f111ebd8f8296517b61d2d23a0207b
awaji-daisuke <awaji_daisuke@intec.co.jp>
2018-06-18 12:54:22 +0900

    mailのプレビューをみる

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,8 @@
 class UserMailer < ApplicationMailer
 
-  def account_activation
+  def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"  
+    mail to: user.email, subject: "Account activation"
   end
 
   def password_reset
