2baada4b80f967e0f6e01d6db05eaac58f66385a
awaji-daisuke <awaji_daisuke@intec.co.jp>
2018-06-25 12:56:34 +0900

    11.3.2

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,8 +1,15 @@
 class AccountActivationsController < ApplicationController
-
-
-private
-  def create_activation_digest
-    # 有効化トークンとダイジェストを作成および代入する
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.update_attribute(:activated,    true)
+      user.update_attribute(:activated_at, Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,16 @@ class SessionsController < ApplicationController
     # メールアドレスはdowncaseで登録してあります
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      # userをsessionに格納する
-      log_in @user
-      # remenberメソッドを使って、ログイン情報を記録させる
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # ユーザログイン後にデフォルトではユーザ情報のページにリダイレクトする。
-      # sessionにurlが保存されていた場合は、そのページにリダイレクトする。
-      redirect_back_or @user
+      if @user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # エラーメッセージを表示する
       flash.now[:danger] = 'Invalid email/password combination'
