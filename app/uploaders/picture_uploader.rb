4c884e7e1fb7e88ffc79dcec3480585aab864e38
daisuke.awaji <daisuke.awaji@i.softbank.jp>
2018-09-05 12:24:00 +0900

    file

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -8,8 +8,6 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  storage :file
-
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
